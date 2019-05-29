2a7a1f2b40ade40abf42e85c0faf6e13566cf49c
gataulova <ev_gataulova@outlook.com>
2019-05-29 21:09:55 +0600

    Fixed select component

--- libs/ui/src/lib/select/select.component.ts
+++ libs/ui/src/lib/select/select.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./select.component.scss"]
 })
 export class SelectComponent implements OnInit {
+  name = "name";
+  value = "value";
+  options = [];
+
   constructor() {}
 
   ngOnInit() {}
+
+  onSelect(option) {}
 }
